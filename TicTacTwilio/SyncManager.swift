cae91f8a61fb71b164f0e529fdf5a9862a8a617e
Gray-Wind <ilya.kolo@gmail.com>
2021-01-18 17:53:45 +0200

    Update code for Sync SDK 1.0.0

--- TicTacTwilio/SyncManager.swift
+++ TicTacTwilio/SyncManager.swift
@@ -23,7 +23,7 @@ class SyncManager: NSObject {
         let properties = TwilioSyncClientProperties()
         if let token = token {
             TwilioSyncClient.syncClient(withToken: token, properties: properties, delegate: self, completion: { (result, syncClient) in
-                if !(result.isSuccessful()) {
+                if !result.isSuccessful {
                     print("TTT: error creating client: \(String(describing: result.error))")
                     completion(nil)
                 } else {
